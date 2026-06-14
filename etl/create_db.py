982a311a889123763480bfea2d35c1587c037e92
akshat7878 <akshatkumawat178@gmail.com>
2026-06-14 19:55:20 +0530

    created the database and check the tables

--- etl/create_db.py
+++ etl/create_db.py
@@ -1,5 +1,16 @@
 import sqlite3
 
 conn = sqlite3.connect("jobs.db")
-print("Database created successfully")
+
+cursor = conn.cursor()
+
+with open("sql/schema.sql", "r") as file:
+    sql_script = file.read()
+
+cursor.executescript(sql_script)
+
+conn.commit()
+
+print("Database and tables created successfully")
+
 conn.close()
